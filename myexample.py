9d62f9d00f694bd3232caa11335f9d10e95dbd74
EOrcun <73969330+EOrcun@users.noreply.github.com>
2021-02-09 20:36:17 +0300

    Removed Some line

--- myexample.py
+++ myexample.py
@@ -1,11 +1,6 @@
-# print('hello world')
-# print('new')
-# print('Visual Studio')
-
-import math
-import os
-
 import requests
 
-name = input("Your  Name?")
-print("Hello,", name)
+
+r = requests.get("https://coreyms.com")
+
+print(r.status_code)
